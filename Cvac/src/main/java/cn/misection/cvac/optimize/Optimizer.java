9c399b64510cad9b3e12ac7f05704a8167d6941a
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-02-15 17:00:27 +0800

    重构;

--- Cvac/src/main/java/cn/misection/cvac/optimize/Optimizer.java
+++ Cvac/src/main/java/cn/misection/cvac/optimize/Optimizer.java
@@ -1,35 +1,36 @@
-//package cn.misection.cvac.optimize;
-//
-//import cn.misection.cvac.ast.FrontAst;
-//
-///**
-// * Created by Mengxu on 2017/1/31.
-// */
-//public class Optimizer
-//{
-//    public void optimize(FrontAst.Program.T prog)
-//    {
-//        UnUsedVarDel varDeler = new UnUsedVarDel();
-//        varDeler.givesWarning = true;
-//        ConstantFolder folder = new ConstantFolder();
-//        UnReachableDel deler = new UnReachableDel();
-//        DeadCodeDel deadDeler = new DeadCodeDel();
-//        ConstantAndCopyPropagation proper = new ConstantAndCopyPropagation();
-//
-//        boolean flag;
-//        do
-//        {
-//            varDeler.visit(prog);
-//            varDeler.givesWarning = false;
-//            folder.visit(prog);
-//            deler.visit(prog);
-//            deadDeler.visit(prog);
-//            proper.visit(prog);
-//            flag = varDeler.isOptimizing()
-//                    || folder.isOptimizing()
-//                    || deler.isOptimizing()
-//                    || deadDeler.isOptimizing()
-//                    || proper.isOptimizing();
-//        } while (flag);
-//    }
-//}
+package cn.misection.cvac.optimize;
+
+import cn.misection.cvac.ast.FrontAst;
+import cn.misection.cvac.ast.program.CvaProgram;
+
+/**
+ * Created by Mengxu on 2017/1/31.
+ */
+public class Optimizer
+{
+    public void optimize(CvaProgram cvaProgram)
+    {
+        UnUsedVarDel varDeler = new UnUsedVarDel();
+        varDeler.givesWarning = true;
+        ConstantFolder folder = new ConstantFolder();
+        UnReachableDel deler = new UnReachableDel();
+        DeadCodeDel deadDeler = new DeadCodeDel();
+        ConstantAndCopyPropagation proper = new ConstantAndCopyPropagation();
+
+        boolean flag;
+        do
+        {
+            varDeler.visit(cvaProgram);
+            varDeler.givesWarning = false;
+            folder.visit(cvaProgram);
+            deler.visit(cvaProgram);
+            deadDeler.visit(cvaProgram);
+            proper.visit(cvaProgram);
+            flag = varDeler.isOptimizing()
+                    || folder.isOptimizing()
+                    || deler.isOptimizing()
+                    || deadDeler.isOptimizing()
+                    || proper.isOptimizing();
+        } while (flag);
+    }
+}
--- Cvac/src/main/java/cn/misection/cvac/optimize/UnUsedVarDel.java
+++ Cvac/src/main/java/cn/misection/cvac/optimize/UnUsedVarDel.java
@@ -1,196 +1,233 @@
-//package cn.misection.cvac.optimize;
-//
-//import cn.misection.cvac.ast.FrontAst;
-//
-//import java.util.Hashtable;
-//
-///**
-// * Created by Mengxu on 2017/1/24.
-// */
-//public class UnUsedVarDel implements cn.misection.cvac.ast.Visitor, Optimizable
-//{
-//    private Hashtable<String, FrontAst.Decl.CvaDeclaration> unUsedLocals;
-//    private Hashtable<String, FrontAst.Decl.CvaDeclaration> unUsedArgs;
-//    private boolean isOptimizing;
-//    public boolean givesWarning;
-//
-//    @Override
-//    public void visit(FrontAst.Type.CvaBoolean t) {}
-//
-//    @Override
-//    public void visit(FrontAst.Type.CvaClassType t) {}
-//
-//    @Override
-//    public void visit(FrontAst.Type.CvaInt t) {}
-//
-//    @Override
-//    public void visit(FrontAst.Decl.CvaDeclaration d) {}
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaAddExpr e)
-//    {
-//        this.visit(e.left);
-//        this.visit(e.right);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaAndAndExpr e)
-//    {
-//        this.visit(e.left);
-//        this.visit(e.right);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaCallExpr e)
-//    {
-//        this.visit(e.exp);
-//        e.args.forEach(this::visit);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaFalseExpr e) {}
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaIdentifier e)
-//    {
-//        if (this.unUsedLocals.containsKey(e.literal))
-//            this.unUsedLocals.remove(e.literal);
-//        else if (this.unUsedArgs.containsKey(e.literal))
-//            this.unUsedArgs.remove(e.literal);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaLTExpr e)
-//    {
-//        this.visit(e.left);
-//        this.visit(e.right);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaNewExpr e) {}
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaNegateExpr e)
-//    {
-//        this.visit(e.expr);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaNumberInt e) {}
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaSubExpr e)
-//    {
-//        this.visit(e.left);
-//        this.visit(e.right);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaThisExpr e) {}
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaMuliExpr e)
-//    {
-//        this.visit(e.left);
-//        this.visit(e.right);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Expr.CvaTrueExpr e) {}
-//
-//    @Override
-//    public void visit(FrontAst.Stm.CvaAssign s)
-//    {
-//        this.visit(new FrontAst.Expr.CvaIdentifier(s.id, s.lineNum));
-//        this.visit(s.exp);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Stm.CvaBlock s)
-//    {
-//        s.stms.forEach(this::visit);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Stm.CvaIfStatement s)
-//    {
-//        this.visit(s.condition);
-//        this.visit(s.thenStm);
-//        this.visit(s.elseStm);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Stm.CvaWriteOperation s)
-//    {
-//        this.visit(s.exp);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Stm.CvaWhileStatement s)
-//    {
-//        this.visit(s.condition);
-//        this.visit(s.body);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Method.CvaMethod m)
-//    {
-//        this.unUsedLocals = new Hashtable<>();
-//        m.locals.forEach(local ->
-//        {
-//            FrontAst.Decl.CvaDeclaration l = (FrontAst.Decl.CvaDeclaration) local;
-//            this.unUsedLocals.put(l.literal, l);
-//        });
-//
-//        this.unUsedArgs = new Hashtable<>();
-//        m.formals.forEach(formal ->
-//        {
-//            FrontAst.Decl.CvaDeclaration f = (FrontAst.Decl.CvaDeclaration) formal;
-//            this.unUsedArgs.put(f.literal, f);
-//        });
-//
-//        m.stms.forEach(this::visit);
-//        this.visit(m.retExp);
-//
-//        this.isOptimizing = this.unUsedArgs.size() > 0
-//                || this.unUsedLocals.size() > 0;
-//        this.unUsedArgs.forEach((uak, uao) ->
-//        {
-//            if (givesWarning)
-//                System.out.println("Warning: at line " + uao.lineNum + " : "
-//                        + "the argument \"" + uak + "\" of method \""
-//                        + m.literal + "\" you have never used.");
-//        });
-//
-//        this.unUsedLocals.forEach((ulk, ulo) ->
-//        {
-//            if (givesWarning)
-//                System.out.println("Warning: at line " + ulo.lineNum + " : "
-//                        + "the local variable \"" + ulk + "\" you have never used."
-//                        + " Now we delete it.");
-//            m.locals.remove(ulo);
-//        });
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.Clas.CvaClass c)
-//    {
-//        c.methods.forEach(this::visit);
-//    }
-//
-//    @Override
-//    public void visit(FrontAst.MainClass.CvaEntry c) {}
-//
-//    @Override
-//    public void visit(FrontAst.Program.CvaProgram p)
-//    {
-//        this.isOptimizing = false;
-//        p.classes.forEach(this::visit);
-//    }
-//
-//    @Override
-//    public boolean isOptimizing()
-//    {
-//        return this.isOptimizing;
-//    }
-//}
+package cn.misection.cvac.optimize;
+
+import cn.misection.cvac.ast.clas.CvaClass;
+import cn.misection.cvac.ast.decl.CvaDeclaration;
+import cn.misection.cvac.ast.entry.CvaEntry;
+import cn.misection.cvac.ast.expr.*;
+import cn.misection.cvac.ast.method.CvaMethod;
+import cn.misection.cvac.ast.program.CvaProgram;
+import cn.misection.cvac.ast.statement.*;
+import cn.misection.cvac.ast.type.CvaBoolean;
+import cn.misection.cvac.ast.type.CvaClassType;
+import cn.misection.cvac.ast.type.CvaInt;
+
+import java.util.Hashtable;
+
+/**
+ * Created by Mengxu on 2017/1/24.
+ */
+public class UnUsedVarDel implements cn.misection.cvac.ast.Visitor, Optimizable
+{
+    private Hashtable<String, CvaDeclaration> unUsedLocals;
+    private Hashtable<String, CvaDeclaration> unUsedArgs;
+    private boolean isOptimizing;
+    public boolean givesWarning;
+
+    @Override
+    public void visit(CvaBoolean t)
+    {
+    }
+
+    @Override
+    public void visit(CvaClassType t)
+    {
+    }
+
+    @Override
+    public void visit(CvaInt t)
+    {
+    }
+
+    @Override
+    public void visit(CvaDeclaration d)
+    {
+    }
+
+    @Override
+    public void visit(CvaAddExpr e)
+    {
+        this.visit(e.getLeft());
+        this.visit(e.getRight());
+    }
+
+    @Override
+    public void visit(CvaAndAndExpr e)
+    {
+        this.visit(e.getLeft());
+        this.visit(e.getRight());
+    }
+
+    @Override
+    public void visit(CvaCallExpr e)
+    {
+        this.visit(e.getExpr());
+        e.getArgs().forEach(this::visit);
+    }
+
+    @Override
+    public void visit(CvaFalseExpr e)
+    {
+    }
+
+    @Override
+    public void visit(CvaIdentifier e)
+    {
+        if (this.unUsedLocals.containsKey(e.getLiteral()))
+        {
+            this.unUsedLocals.remove(e.getLiteral());
+        }
+        else if (this.unUsedArgs.containsKey(e.getLiteral()))
+        {
+            this.unUsedArgs.remove(e.getLiteral());
+        }
+    }
+
+    @Override
+    public void visit(CvaLTExpr e)
+    {
+        this.visit(e.getLeft());
+        this.visit(e.getRight());
+    }
+
+    @Override
+    public void visit(CvaNewExpr e)
+    {
+    }
+
+    @Override
+    public void visit(CvaNegateExpr e)
+    {
+        this.visit(e.getExpr());
+    }
+
+    @Override
+    public void visit(CvaNumberInt e)
+    {
+    }
+
+    @Override
+    public void visit(CvaSubExpr e)
+    {
+        this.visit(e.getLeft());
+        this.visit(e.getRight());
+    }
+
+    @Override
+    public void visit(CvaThisExpr e)
+    {
+    }
+
+    @Override
+    public void visit(CvaMuliExpr e)
+    {
+        this.visit(e.getLeft());
+        this.visit(e.getRight());
+    }
+
+    @Override
+    public void visit(CvaTrueExpr e)
+    {
+    }
+
+    @Override
+    public void visit(CvaAssign s)
+    {
+        this.visit(new CvaIdentifier(s.getLineNum(), s.getLiteral()));
+        this.visit(s.getExpr());
+    }
+
+    @Override
+    public void visit(CvaBlock s)
+    {
+        s.getStatementList().forEach(this::visit);
+    }
+
+    @Override
+    public void visit(CvaIfStatement s)
+    {
+        this.visit(s.getCondition());
+        this.visit(s.getThenStatement());
+        this.visit(s.getElseStatement());
+    }
+
+    @Override
+    public void visit(CvaWriteOperation s)
+    {
+        this.visit(s.getExpr());
+    }
+
+    @Override
+    public void visit(CvaWhileStatement s)
+    {
+        this.visit(s.getCondition());
+        this.visit(s.getBody());
+    }
+
+    @Override
+    public void visit(CvaMethod m)
+    {
+        this.unUsedLocals = new Hashtable<>();
+        m.getLocalList().forEach(local ->
+        {
+            CvaDeclaration l = (CvaDeclaration) local;
+            this.unUsedLocals.put(l.getLiteral(), l);
+        });
+
+        this.unUsedArgs = new Hashtable<>();
+        m.getFormalList().forEach(formal ->
+        {
+            CvaDeclaration f = (CvaDeclaration) formal;
+            this.unUsedArgs.put(f.getLiteral(), f);
+        });
+
+        m.getStatementList().forEach(this::visit);
+        this.visit(m.getRetExpr());
+
+        this.isOptimizing = this.unUsedArgs.size() > 0
+                || this.unUsedLocals.size() > 0;
+        this.unUsedArgs.forEach((uak, uao) ->
+        {
+            if (givesWarning)
+            {
+                System.out.printf("Warning: at line %d : the argument \"%s\" of" +
+                                " method \"%s\" you have never used.%n",
+                        uao.getLineNum(), uak, m.getLiteral());
+            }
+        });
+
+        this.unUsedLocals.forEach((ulk, ulo) ->
+        {
+            if (givesWarning)
+            {
+                System.out.printf("Warning: at line %d : the local variable " +
+                                "\"%s\" you have never used. Now we delete it.%n",
+                        ulo.getLineNum(), ulk);
+            }
+            m.getLocalList().remove(ulo);
+        });
+    }
+
+    @Override
+    public void visit(CvaClass c)
+    {
+        c.getMethodList().forEach(this::visit);
+    }
+
+    @Override
+    public void visit(CvaEntry c)
+    {
+    }
+
+    @Override
+    public void visit(CvaProgram p)
+    {
+        this.isOptimizing = false;
+        p.getClassList().forEach(this::visit);
+    }
+
+    @Override
+    public boolean isOptimizing()
+    {
+        return this.isOptimizing;
+    }
+}
