19acdf826acb63079795bb1158d2aae016ae9729
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-02-19 14:01:00 +0800

    取消了bufferHandler与bufferedReader的继承关系;

--- Cvac/src/main/java/cn/misection/cvac/config/Macro.java
+++ Cvac/src/main/java/cn/misection/cvac/config/Macro.java
@@ -4,7 +4,7 @@ package cn.misection.cvac.config;
  * @author Military Intelligence 6 root
  * @version 1.0.0
  * @ClassName Macro
- * @Description TODO
+ * @Description 版本发行, 用于条件编译;
  * @CreateTime 2021年02月13日 23:37:00
  */
 public final class Macro
