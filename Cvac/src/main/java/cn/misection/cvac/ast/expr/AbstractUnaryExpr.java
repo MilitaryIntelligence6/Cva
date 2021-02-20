e6f3642f0bb43bea37f0bdbd987230bd3a87784a
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-02-20 20:09:24 +0800

    基本类型重构成了枚举, 变得比较优雅;

---
+++ Cvac/src/main/java/cn/misection/cvac/ast/expr/AbstractUnaryExpr.java
@@ -0,0 +1,12 @@
+package cn.misection.cvac.ast.expr;
+
+/**
+ * @author Military Intelligence 6 root
+ * @version 1.0.0
+ * @ClassName AbstractUnaryExpr
+ * @Description TODO
+ * @CreateTime 2021年02月20日 20:06:00
+ */
+public abstract class AbstractUnaryExpr
+{
+}
