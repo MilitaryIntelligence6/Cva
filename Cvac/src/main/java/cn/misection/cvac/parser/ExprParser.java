4e35bbbe637674c5717b7b61a8098a6e1230761e
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-03-16 16:56:21 +0800

    原机器坏了, 重新部署新计算机后的一些notify;

---
+++ Cvac/src/main/java/cn/misection/cvac/parser/ExprParser.java
@@ -0,0 +1,14 @@
+package cn.misection.cvac.parser;
+
+/**
+ * @author Military Intelligence 6 root
+ * @version 1.0.0
+ * @ClassName ParseExpr
+ * @Description TODO
+ * @CreateTime 2021年03月13日 14:52:00
+ */
+@FunctionalInterface
+public interface ExprParser
+{
+    void parse();
+}
